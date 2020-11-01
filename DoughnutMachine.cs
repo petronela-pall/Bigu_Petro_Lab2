a8708a6870070dd8e51d0340c9f0024c33ecf21f
Petronela Pall <petronela.bigu@yahoo.com>
2020-11-01 10:46:15 +0200

    doughnut machin

---
+++ DoughnutMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bigu_Petro_Lab2
+{
+    class Doughnut.Machine
+    {
+    }
+public enum DoughnutType
+{
+    Glazed,
+    Sugar,
+    Lemon,
+    Chocolate,
+    Vanilla
+}
+}
